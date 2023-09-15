5a6a94aca211acb58ed7730d95e01e5b56e11ae8
Leon Klute <l.klute@caesar.nl>
2023-09-15 13:39:46 +0200

    main: fix tests

--- src/app/item.service.spec.ts
+++ src/app/item.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ItemService } from './item.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ItemService', () => {
   let service: ItemService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ItemService);
   });
 
--- src/app/scanner/scanner.component.spec.ts
+++ src/app/scanner/scanner.component.spec.ts
@@ -1,18 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ScannerComponent } from './scanner.component';
+import {ScannerComponent} from './scanner.component';
 import {CustomerService} from "../customer.service";
 import {FormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {ItemService} from "../item.service";
 
 class CustomerServiceMock {
   getObserver() {
     return {
-      subscribe: () => {}
+      subscribe: () => {
+      }
     };
   }
 }
+
+class ItemServiceMock {
+  getItem() {
+    return {
+      subscribe: () => {
+      }
+    };
+  }
+}
+
 describe('ScannerComponent', () => {
   let component: ScannerComponent;
   let fixture: ComponentFixture<ScannerComponent>;
@@ -20,7 +32,11 @@ describe('ScannerComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ScannerComponent],
-      providers: [{ provide: CustomerService, useClass: CustomerServiceMock }],
+      providers: [{
+        provide: CustomerService, useClass: CustomerServiceMock,
+      }, {
+        provide: ItemService, useClass: ItemServiceMock
+      }],
       imports: [FormsModule, MatInputModule, NoopAnimationsModule]
     });
     fixture = TestBed.createComponent(ScannerComponent);
